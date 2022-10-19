cdeb2d40ef40b9d4a69056a10e16d5bd695d0875
Josh Norman <joshua.norman@rallyhealth.com>
2022-10-19 13:34:57 -0700

    updated conf

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -21,8 +18,7 @@ release = 'v0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.githubpages'
+    'sphinx.ext.napoleon'
 ]
 
 templates_path = ['_templates']
@@ -34,4 +30,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
