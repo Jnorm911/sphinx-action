3f9da060a04f4143cbecd9904aa0a6df7abfb8c3
Josh Norman <joshua.norman@rallyhealth.com>
2022-10-26 19:02:32 -0700

    adt3

--- docs/conf.py
+++ docs/conf.py
@@ -1,31 +1,22 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'Sphinx Docs'
 copyright = '2022, Josh Norman'
 author = 'Josh Norman'
 release = 'v0.7'
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.napoleon'
 ]
-
 autosummary_generate = True
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = []
--- docs/index.rst
+++ docs/index.rst
@@ -1,16 +1,10 @@
-.. Sphinx Docs documentation master file, created by
-   sphinx-quickstart on Wed Oct 26 17:21:19 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to Sphinx Docs's documentation!
-=======================================
+PROJECT
+=======
 
 .. toctree::
-   :maxdepth: 4
-   :caption: Contents:
 
-.. include:: modules.rst
+.. autosummary::
+   :toctree: _autosummary
 
 Indices and tables
 ==================
