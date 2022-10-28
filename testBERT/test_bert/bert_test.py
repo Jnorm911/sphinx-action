394a4c5b7db14ca1eefa4fa0e6135c1a2df8a644
Josh Norman <joshua.norman@rallyhealth.com>
2022-10-28 15:29:41 -0700

    append0

--- testBERT/test_bert/bert_test.py
+++ testBERT/test_bert/bert_test.py
@@ -1,3 +1,6 @@
+import os
+import pandas as pd
+
 def subtraction(a,b):
    """Simple subtraction function
 
@@ -10,3 +13,13 @@ def subtraction(a,b):
    """
    c = a-b
    return c
+
+
+def load_data(LOADDATES, data_path, filename, cols=None):
+    """ Hello World
+    """
+    datals = []  
+    data = pd.read_csv(os.path.join(data_path, filename)).fillna("Unknown")
+    data = data[cols]
+    datals.info(data.shape)
+    return data
