799ddcef33e01959fe3ce380072ca2a5677abcdb
Josh Norman <joshua.norman@rallyhealth.com>
2022-10-28 14:56:47 -0700

    ph6

--- testBERT/test_bert/bert_test.py
+++ testBERT/test_bert/bert_test.py
@@ -1,42 +1,12 @@
-import os
-import pandas as pd
-import logging
-
-
-
-def load_data(LOADDATES, data_path, filename, cols=None):
-    """ Hello World
-    """
-    datals = []
-    
-    # for t, dt in enumerate(LOADDATES):
-    #     FNAME = os.path.join(train_data_path, 'claim_l_{}.csv.gz'.format(dt))
-    #     try:
-    #         datals.append(pd.read_csv(FNAME, usecols = cols, low_memory=False).fillna('Unknown'))
-    #     except:
-    #         logger.info('File {} missing'.format( FNAME))
-    #         continue
-    #     logger.info('{} of {}, file {}'.format(t+1, len(LOADDATES), FNAME))
-    
-    data = pd.read_csv(os.path.join(data_path, filename)).fillna("Unknown")
-    # data = pd.concat(datals, 0)
-    # logger.info('check patient before sorting {}'.format(data[['PatientId','BegDateOfService','LineId']].head(20)))
-    # data = data.sort_values(['PatientId','BegDateOfService','LineId'])
-    # logger.info('check patient sorting {}'.format(data[['PatientId','BegDateOfService','LineId']].head(20)))
-    # data = data.head(100000)
-    data = data[cols]
-    logging.info(data.shape)
-    return data
-
 def subtraction(a,b):
-    """ Subtraction Function
+   """Simple subtraction function
 
-    Args:
-        a (int): first user input
-        b (int): second user input
+   Args:
+       a (int): int from user input
+       b (int): int from user input
 
-    Returns:
-        int: c, a-b
-    """
-    c = a-b
-    return c
+   Returns:
+       int: c, the subtraction of user input a - b
+   """
+   c = a-b
+   return c
