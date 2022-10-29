a029124ecc6c64468c4bd4a9d33d02942bdb4827
Josh Norman <joshua.norman@rallyhealth.com>
2022-10-28 19:03:31 -0700

    ph6

--- testBERT/test_bert/bert_test.py
+++ testBERT/test_bert/bert_test.py
@@ -1,6 +1,3 @@
-import os
-# import pandas as pd
-
 def subtraction(a,b):
    """Simple subtraction function
 
@@ -27,23 +24,5 @@ def load_data(LOADDATES, data_path, filename, cols=None):
     Returns:
         Series: pandas data
     """
-    datals = []
-    
-    # for t, dt in enumerate(LOADDATES):
-    #     FNAME = os.path.join(train_data_path, 'claim_l_{}.csv.gz'.format(dt))
-    #     try:
-    #         datals.append(pd.read_csv(FNAME, usecols = cols, low_memory=False).fillna('Unknown'))
-    #     except:
-    #         logger.info('File {} missing'.format( FNAME))
-    #         continue
-    #     logger.info('{} of {}, file {}'.format(t+1, len(LOADDATES), FNAME))
-    
-    data = pd.read_csv(os.path.join(data_path, filename)).fillna("Unknown")
-    # data = pd.concat(datals, 0)
-    # logger.info('check patient before sorting {}'.format(data[['PatientId','BegDateOfService','LineId']].head(20)))
-    # data = data.sort_values(['PatientId','BegDateOfService','LineId'])
-    # logger.info('check patient sorting {}'.format(data[['PatientId','BegDateOfService','LineId']].head(20)))
-    # data = data.head(100000)
-    data = data[cols]
-    # logging.info(data.shape)
+    data = []
     return data
